ff6309dc2fe40cc8ecfe3f481e1d0edbd166a3ad
Casey Johnson <johnsoncm3@gmail.com>
2021-04-25 23:43:11 -0400

    installed dependencies

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+// Packages needed for this application
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 //User prompts go here
+
+const questions = [
+    {
+        type: 'list',
+        name: 'type',
+        message: 'What would you like to build?',
+        choices: ['Manager' , 'Engineer' , 'Intern'],
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your title?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your employee id?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is your office number?',
+
+    },
+
+]
+
+inquirer
+    .prompt(questions)
+    
